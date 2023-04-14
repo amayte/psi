1b206cd8fe8c6bb6abf71859f3db0a87df18590d
Maytê <mayte.magri@gmail.com>
2023-04-14 17:54:27 -0300

    Semi-pronto. Falta arrumar a animação das tabs.

--- sendEmail.php
+++ sendEmail.php
@@ -1,17 +1,8 @@
 <?php
-
-$for = "mayte.magri@outlook.com";
-$name = $_POST['name'];
-$message = "<strong>Nome:  </strong>".$name;
-$message .= "<br>  <strong>Mensagem: </strong>"
-.$_POST['message'];
-$headers =  "Content-Type:text/html; charset=UTF-8\n";
-$headers .= "From:  dominio.com.br<sistema@dominio.com.br>\n";
-$headers .= "X-Sender:  <sistema@dominio.com.br>\n";
-$headers .= "X-Mailer: PHP  v".phpversion()."\n";
-$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-$headers .= "Return-Path:  <sistema@dominio.com.br>\n";
-$headers .= "MIME-Version: 1.0\n";
-
-mail($for, $message, $headers);
+$to = "mayte.magri@outlook.com";
+$subject = "Mensagem do formulário de contato";
+$name = $_POST["name"];
+$message = $_POST["message"];
+$headers = "From: email-do-remetente@seu-site.com" . "\r\n";
+mail($to, $subject, $message, $headers);
 ?>
